849112f4cdc473b43e68e475e67d9b7e20d1413c
Esshka <esshka@gmail.com>
2016-03-20 23:17:28 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Minimal webpack boilerplate
+* react
+* webpack dev-server
+* hot module replacement
+
+1. npm install
+2. npm start - development (http://localhost:3000 - dev server)
+3. npm run build - build dist 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <img src="i/test.png" alt=""/>
+          <h1>Hello, App!</h1>
+      </div>
+    );
+  }
+}
